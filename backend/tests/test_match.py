50526b0555e7b533ea6858ca1707b81b3c0c8dbb
Ulmyoc <taylor.ulman11@gmail.com>
2026-05-18 00:38:04 -0700

    Finished matching logic for courses, availability, and preferences

--- backend/tests/test_match.py
+++ backend/tests/test_match.py
@@ -1,21 +1,60 @@
 import pytest
-from app.matching import calculate_matching_score
+from app.matching import get_final_score
 
 def test_perfect_match():
-    user_a  = ["CS35L", "MATH131A"]
-    user_b  = ["CS35L", "MATH131A"]
-    assert calculate_matching_score(user_a, user_b) == 100.0
+    user_a = {
+        "courses": ["CS35L", "MATH131A"],
+        "availability": ["M12", "W14"],
+        "preferences": ["Quiet"]
+    }
+
+    user_b = {
+        "courses": ["CS35L", "MATH131A"],
+        "availability": ["M12", "W14"],
+        "preferences": ["Quiet"]
+    }
+
+    assert get_final_score(user_a, user_b) == 100.0
 
 def test_partial_match():
-    user_a  = [ "MATH131A", "MATH170E"]
-    user_b  = ["CS35L", "MATH131A"]
-    assert calculate_matching_score(user_a, user_b) == 33.33
+    user_a = {
+        "courses": ["CS35L"],
+        "availability": ["W14"],
+        "preferences": ["Quiet"]
+    }
+
+    user_b = {
+        "courses": ["CS35L"],
+        "availability": ["M12"],
+        "preferences": ["Quiet"]
+    }
+    
+    assert get_final_score(user_a, user_b) == 70.0
 
 def test_no_match():
-    user_a  = ["Math168"]
-    user_b  = ["MATH131A"]
-    assert calculate_matching_score(user_a, user_b) == 0.0
+    user_a = {
+        "courses" : ["CS35L"],
+        "availability": ["M10"],
+        "preferences": ["Quiet"]
+    }
+
+    user_b = {
+        "courses" : ["Math116"],
+        "availability": ["W10"],
+        "preferences": ["Coffee Shop"]
+    }
+    assert get_final_score(user_a, user_b) == 0.0
 
 def test_no_data():
-    assert calculate_matching_score([], []) == 0.0
-    assert calculate_matching_score(["CS35L"], []) == 0.0
+    user_a = {
+        "courses" : [],
+        "availability": [],
+        "preferences": []
+    }
+
+    user_b = {
+        "courses" : [],
+        "availability": [],
+        "preferences": []
+    }
+    assert get_final_score(user_a, user_b) == 0.0
